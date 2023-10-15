47b38e319a771f2e62eb11592730e9878ad4da45
darecoco <whaqlrh37@naver.com>
2023-10-15 22:13:58 +0900

    이미지 아래로 움직임 성공

--- Witch's_money/src/rhythmGame/MoveMonster.java
+++ Witch's_money/src/rhythmGame/MoveMonster.java
@@ -1,11 +1,14 @@
 package rhythmGame;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class MoveMonster{
 	/* 처음 줄 690 800
@@ -14,6 +17,7 @@ public class MoveMonster{
 	 * 네 번째 줄 1097 800*/
 	private ImageIcon monster;
 	private JLabel enemy;
+	private Timer timer;
 	
 	public MoveMonster(JPanel bg, int x, int y) {
 		String imagePath = "images/rhythm/enemy/red.png";
@@ -30,9 +34,30 @@ public class MoveMonster{
         enemy.setBounds(x, y, monster.getIconWidth(), monster.getIconHeight());
         bg.add(enemy);
         bg.repaint();
+        
+     // 타이머를 사용하여 이미지 이동
+        timer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	int move_y = enemy.getY();
+                // y 좌표를 업데이트하여 이미지 이동
+                move_y += 5; // y 좌표를 10 픽셀씩 이동
+
+                // 이미지의 위치를 업데이트
+                enemy.setLocation(x, move_y);
+
+                // 화면 다시 그리기
+                bg.repaint();
+            }
+        });
+
 	}
 	
 	JLabel getRed() {
 		return enemy;
+	}
+	
+	void moveStart() {
+		timer.start();
 	}
 }
--- Witch's_money/src/rhythmGame/Rhythm_main.java
+++ Witch's_money/src/rhythmGame/Rhythm_main.java
@@ -9,5 +9,7 @@ public class Rhythm_main{
 		MoveMonster enemy2 = new MoveMonster(bg.getBG(), 825, -50);
 		MoveMonster enemy3 = new MoveMonster(bg.getBG(), 960, -10);
 		MoveMonster enemy4 = new MoveMonster(bg.getBG(), 1097, 0);
+		
+		enemy.moveStart();
 	}
 }
