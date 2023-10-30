77aebd80bb0c012aaa6a8a681c2885fb1a8ba666
darecoco <whaqlrh37@naver.com>
2023-10-31 08:39:32 +0900

    배경 바꾸는 메서드 만듦

--- Witch's_money/src/ver0_0_1/Rhythm_bg.java
+++ Witch's_money/src/ver0_0_1/Rhythm_bg.java
@@ -1,31 +1,32 @@
 package ver0_0_1;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.io.File;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Rhythm_bg{
-	private ImageIcon icon;
+	private Image screenBG;
 	private JPanel background;
 	
 	public Rhythm_bg(JFrame base, String room) {
 		String imagePath = "images/rhythm/bg/" + room + ".png";
 		File img = new File(imagePath);
 		if(img.isFile()) {
-			icon = new ImageIcon(imagePath);
+			screenBG = new ImageIcon(imagePath).getImage();
 		}else {
 			JOptionPane.showMessageDialog(null, "이미지 로딩 오류");
 			System.exit(0);
 		}
-       
-        //배경 Panel 생성후 컨텐츠페인으로 지정      
+		
+		//배경 Panel 생성후 컨텐츠페인으로 지정      
         background = new JPanel() {
             public void paintComponent(Graphics g) {
-                g.drawImage(icon.getImage(), 0, 0, this);
+                g.drawImage(screenBG, 0, 0, this);
                 setOpaque(false);
                 super.paintComponent(g);
             }
@@ -38,5 +39,30 @@ public class Rhythm_bg{
 	
 	JPanel getBG() { //JPanel background
 		return background;
+	}
+	
+	void changeBG(JFrame base, String room) {
+		
+		String imagePath = "images/rhythm/bg/" + room + ".png";
+		File img = new File(imagePath);
+		if(img.isFile()) {
+			screenBG = new ImageIcon(imagePath).getImage();
+		}else {
+			JOptionPane.showMessageDialog(null, "이미지 로딩 오류");
+			System.exit(0);
+		}
+		
+		//배경 Panel 생성후 컨텐츠페인으로 지정      
+        background = new JPanel() {
+            public void paintComponent(Graphics g) {
+                g.drawImage(screenBG, 0, 0, this);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+        };
+        
+        base.setTitle("리듬겜 기본 창");
+        base.setContentPane(background);
+        base.setVisible(true);
 	}
 }
