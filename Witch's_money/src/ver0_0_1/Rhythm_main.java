0cb7e761884e59629ac28841f9a0e81926ebe5de
darecoco <whaqlrh37@naver.com>
2023-11-02 23:32:03 +0900

    [테스트] 숫자 노트로 노트 구현

--- Witch's_money/src/ver0_0_1/Rhythm_main.java
+++ Witch's_money/src/ver0_0_1/Rhythm_main.java
@@ -7,19 +7,40 @@ import javax.swing.JFrame;
 public class Rhythm_main extends Thread{
 	Rhythm_main(JFrame base) {
 		Rhythm_bg main = new Rhythm_bg(base, "main_game");
-		Rhythm_moveMonster[] enermy = new Rhythm_moveMonster[] {
-				new Rhythm_moveMonster(main.getBG(), 1, 7),
-				new Rhythm_moveMonster(main.getBG(), 2, 7),
-				new Rhythm_moveMonster(main.getBG(), 3, 7),
-				new Rhythm_moveMonster(main.getBG(), 4, 7)
-		};
-		for(Rhythm_moveMonster pls : enermy) {}
+		int plss[] = new int[] {0,1,1,1,1,1,1,1};
+//		Rhythm_moveMonster[] enermy = new Rhythm_moveMonster[] {
+//				new Rhythm_moveMonster(main.getBG(), 1),
+//				new Rhythm_moveMonster(main.getBG(), 2),
+//				new Rhythm_moveMonster(main.getBG(), 3),
+//				new Rhythm_moveMonster(main.getBG(), 4)
+//		};
+		
+		Thread test = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int len = plss.length;
+				for(int i = 0; i < len; i++) {
+					try {
+						Thread.sleep(500);
+						if(plss[i] == 1) {
+							new Rhythm_moveMonster(main.getBG(), 1);
+						}
+					}catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+//		for(Rhythm_moveMonster pls : enermy) {}//생성자만 실행
+		
 		Thread musicThread = new Thread(new Runnable() {
 			@Override
 	        public void run() {
-				Rhythm_selectMusic music = new Rhythm_selectMusic();
+				Rhythm_selectMusic music = new Rhythm_selectMusic(main.getBG());
 			}
 		});
 		musicThread.start();
+		test.start();
 	}
 }
