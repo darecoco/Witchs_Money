08fa1b2d199439c94887e5b62dcb8b98987ff43f
darecoco <whaqlrh37@naver.com>
2023-11-14 00:34:46 +0900

    로비 화면 추가

--- Witch's_money/src/ver0_0_1/Rhythm_bg.java
+++ Witch's_money/src/ver0_0_1/Rhythm_bg.java
@@ -1,17 +1,29 @@
 package ver0_0_1;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Rhythm_bg{
 	private Image screenBG;
 	private JPanel background;
+	private int gameStart = 0;
+	JButton ready;
+	JButton exit;
+	JLabel day;
 	
 	public Rhythm_bg(JFrame base, String room) {
 		String imagePath = "images/rhythm/bg/" + room + ".png";
@@ -65,5 +77,73 @@ public class Rhythm_bg{
         base.setTitle("리듬겜 기본 창");
         base.setContentPane(background);
         base.setVisible(true);
+	}
+	
+	void setLobby() {
+		settingDay();
+		settingLobby();
+	}
+	
+	void settingDay() {
+		File progress = new File("scripts/ect/progress.witchmoney");
+		String days = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(progress.getPath()));
+			days = br.readLine();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		day = new JLabel(days + "일 차");
+		day.setBounds(690, 960, 200, 90);
+		day.setFont(new Font("Nanum SuJubEunDaeHagSaeng", Font.PLAIN, 80));
+		day.setForeground(Color.BLACK);
+		day.setHorizontalAlignment(JLabel.CENTER);
+		
+		background.add(day);
+		background.repaint();
+	}
+	
+	void settingLobby() {
+		ready = new JButton("게임 시작");
+//		JButton help = new JButton("게임 방법");
+		exit = new JButton("나가기");
+		ready.setBounds(1405, 680, 300, 80);
+//		help.setBounds(1405, 820, 300, 80);
+		exit.setBounds(1405, 950, 300, 80);
+		
+		ready.addActionListener(e ->{
+			this.gameStart = 1;
+		});
+//		help.addActionListener(e -> {
+//			return;
+//		});
+		exit.addActionListener(e -> {
+			int select =JOptionPane.showConfirmDialog(background, "정말로 게임에서 나가시겠습니까?", "게임종료", JOptionPane.YES_NO_OPTION);
+			if(select == 0) {
+				System.exit(0);
+			}
+		});
+		
+		background.add(ready);
+//		background.add(help);
+		background.add(exit);
+		
+		background.repaint();
+	}
+	
+	void startGame() {
+		while(gameStart != 1) {
+			try {
+				Thread.sleep(1);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		ready = null;
+		exit = null;
+		day = null;
 	}
 }
