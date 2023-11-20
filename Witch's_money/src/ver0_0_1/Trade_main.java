e19b570b237f4ae8c4b1c3e415440b3e5c5aab64
darecoco <whaqlrh37@naver.com>
2023-11-20 22:35:44 +0900

    무역겜 전체적인 흐름(초반)

--- Witch's_money/src/ver0_0_1/Trade_main.java
+++ Witch's_money/src/ver0_0_1/Trade_main.java
@@ -10,5 +10,18 @@ public class Trade_main {
 		Trade_bg bg = new Trade_bg(base, "choice_market");
 		bg.setBgColor();
 		bg.choiceMarket();
+		String market = bg.watiChoice();
+		bg.changeBG(base, market);
+		Trade_customer customer = new Trade_customer(bg.getBG(), market);
+		customer.comeCustomer();
+		Trade_bgm bgm = new Trade_bgm(market);
+		try {
+			System.out.println("어...");
+			Thread.sleep(10000);
+			bgm.endBgm();
+		}catch(InterruptedException e) {
+			System.out.println("응 끝.");
+		}
+		System.out.println("왜 안끝나악");
 	}
 }
